247339c5d3a279b434e39482b0d473dda404a184
Арман Чархчян <re-mont1@mail.ru>
2022-05-20 18:18:48 +0400

    fix

--- Sources/ModelInterfaces/ModelInterfaces.swift
+++ Sources/ModelInterfaces/ModelInterfaces.swift
@@ -93,3 +93,20 @@ public enum RemovedProfileConstants: String {
     case info = "Пользователь удалил свой профиль"
     case postsCount = "0"
 }
+
+public protocol ProfileNetworkModelProtocol {
+    var userName: String { get set }
+    var info: String { get set }
+    var sex: String { get set }
+    var imageUrl: String { get set }
+    var id: String { get set }
+    var country: String { get set }
+    var city: String { get set }
+    var birthday: String { get set }
+    var removed: Bool { get set }
+    var online: Bool { get set }
+    var lastActivity: Date? { get set }
+    var postsCount: Int { get set }
+    
+    func convertModelToDictionary() -> [String: Any]
+}
